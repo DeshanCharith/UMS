b4639445e284f165e1c697312191a110c4ad0fc0
DeshanCharith <deshancharith2@gmail.com>
2022-04-25 12:35:54 +0530

    Backend initialize with db connection

---
+++ Backend/server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 8081;
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose.connect(MONGODB_URI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}, (error) => {
+
+    if (error) {
+        console.log('Database Error:', error.message);
+        console.log('######################################################');
+    }
+});
+
+mongoose.connection.once('open', () => {
+    console.log('Database Connected...');
+  
+    console.log('######################################################');
+});
+
+app.route('/').get((req, res) => {
+    res.send('SPM Group Project');
+});
+
+
+if(process.env.NODE_ENV === 'production'){
+
+    app.use(express.static('FrontEnd/build'))
+}
+
+
+app.listen(PORT, () => {
+    console.log('######################################################');
+    console.log(`Server is ON and running on PORT : ${PORT}`);
+    console.log('...Wait DB connecting...');
+});
+
