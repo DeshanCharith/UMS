41956bd65f6c7657fdabd36d85cb1fb87a9541f6
DeshanCharith <deshancharith2@gmail.com>
2022-04-26 19:34:16 +0530

    Team lead load to dropdown via API

--- frontend/src/components/Allusers.js
+++ frontend/src/components/Allusers.js
@@ -1,31 +1,76 @@
-import React, {useState,useEffect} from "react";
+import React, {Component}from "react";
 import axios from "axios";
+const initialState = {
+    
+    selectOptions : [],
+    DisplayData : '',
+}
 
-export default function AllUsers(){
+class allUser extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = initialState;
 
-    const [users, setUsers] = useState([]);
+    }
 
-    useEffect(()=> {
-        function getUserData(){       
-            axios.get("http://localhost:8070/user/").then((res)=>{
-                console.log(res.data);
-                setUsers(res.data);
-            }).catch((err)=>{
-                alert(err.message);
-            })
-        }
 
-    getUserData();
-    },[])
+    async getOptions(){
+        const res = await axios.get('http://localhost:8070/user/')
+        const data = res.data
+
+        this.setState({selectOptions: data})
+    
+      }
+
+      componentDidMount(){
+        this.getOptions();
+    }
   
 
-    return(
-        <div>
-           
-         <h2>View Users</h2>
+    render() {
 
 
-        </div>
-          
-    )
+        console.log(this.state.DisplayData);
+        return(
+<div>
+<div class="" style={{width:"100%"}}>
+<h4>View Users</h4><br></br>
+
+<table class="table">
+<thead>
+   <tr>
+   
+   <th scope="col">Name</th>
+   <th scope="col">Email</th>
+   <th scope="col">DOB</th>
+   <th scope="col">Role</th>
+   <th scope="col">Assign Lead</th>
+   <th scope="col">Dept</th>
+   </tr>
+</thead>
+<tbody>
+                 
+                    
+{this.state.selectOptions.map(data=>(
+    <tr>
+                   <td><input name = {data.name} defaultValue={data.name}></input> </td>
+                   <td><input name = {data.email} defaultValue={data.email}></input> </td>
+                   <td><input name = {data.dob} defaultValue={data.dob}></input> </td>
+                   <td><input name = {data.role} defaultValue={data.role}></input> </td>
+                   <td><input name = {data.assign_lead} defaultValue={data.assign_lead}></input> </td>
+                   <td><input name = {data.dept} defaultValue={data.dept}></input> </td>
+ </tr>
+                  )) }
+                    
+                </tbody>
+</table>
+</div>
+</div>
+        )
+
+    }
 }
+
+
+export default  allUser;
